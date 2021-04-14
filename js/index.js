c4a1f373f77c6422fab7127fa2ba2a278a4951c0
onuorah ike <jsxike@gmail.com>
2021-04-14 18:36:54 +0100

    home page finished

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
+// NAVIGATION
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $('.navbar_icon').on('click', () => {
-  $('.navbar_icon').toggleClass('clicked');
+  // $('.navbar_icon').toggleClass('clicked');
+  if ($('.navbar_icon').hasClass('fa-bars')) {
+    $('.navbar_icon').removeClass('fa-bars');
+    $('.navbar_icon').addClass('fa-times clicked');
+  } else {
+    $('.navbar_icon').addClass('fa-bars');
+    $('.navbar_icon').removeClass('fa-times clicked');
+  }
+
   $('.navbar').toggleClass('clicked');
 });
 $('.navbar a').on('click', () => {
   $('.navbar_icon').removeClass('clicked');
   $('.navbar').removeClass('clicked');
 });
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
