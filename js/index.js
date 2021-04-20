ec237d81d93085ecbdb4104b77e2f2a979fa608e
onuorah ike <jsxike@gmail.com>
2021-04-20 04:17:55 +0100

    signin,signup,passreset

--- js/index.js
+++ js/index.js
@@ -26,3 +26,71 @@ $('.faq_icon').on('click', (e) => {
   parent.children[2].classList.toggle('clicked');
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let emailFormat = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/g;
+let passwordFormat = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/g;
+let lowerCaseLetters = /[a-z]/g;
+let upperCaseLetters = /[A-Z]/g;
+let numbers = /[0-9]/g;
+// FORMS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$('#signin_name').on('keyup', () => {
+  if ($('#signin_name').val().length >= 8) {
+    $('#nameverify').html('good!').css('color', 'green');
+  } else {
+    $('#nameverify').html('minimum 8 characters').css('color', 'red');
+  }
+});
+$('#signin_name').on('blur', () => {
+  $('#nameverify').html('');
+});
+
+$('#signin_email').on('keyup', () => {
+  if ($('#signin_email').val().match(emailFormat)) {
+    $('#emailverify').html('good!').css('color', 'green');
+  } else {
+    $('#emailverify').html('follow standard mail format e.g example@mail.com').css('color', 'red');
+  }
+});
+$('#signin_email').on('blur', () => {
+  $('#emailverify').html('');
+});
+
+$('#signin_password').on('keyup', () => {
+  if ($('#signin_password').val().match(passwordFormat)) {
+    $('#passverify').html('good!').css('color', 'green');
+  } else {
+    $('#passverify').html('lowercase, uppercase, number and minimum 8 characters').css('color', 'red');
+  }
+});
+$('#signin_password').on('blur', () => {
+  $('#passverify').html('');
+});
+$('#signin_passwordConfirm').on('keyup', function () {
+  if ($('#signin_password').val() == $('#signin_passwordConfirm').val()) {
+    $('#confirmverify').html('password and password confirm match 🙂').css('color', 'green');
+  } else $('#confirmverify').html('password and password confirm does not match ☹').css('color', 'red');
+});
+$(' #signin_passwordConfirm').on('blur', function () {
+  $('#confirmverify').html('');
+});
+
+$('#hide1').on('click', function () {
+  if ($('#signin_password').attr('type') == 'password') {
+    $('#hide1').removeClass('fa-eye-slash').addClass('fa-eye');
+    $('#signin_password').attr('type', 'text');
+  } else {
+    $('#hide1').removeClass('fa-eye').addClass('fa-eye-slash');
+    $('#signin_password').attr('type', 'password');
+  }
+});
+$('#hide1').on('click', function () {
+  if ($('#signup_password').attr('type') == 'password') {
+    $('#hide1').removeClass('fa-eye-slash').addClass('fa-eye');
+    $('#signup_password').attr('type', 'text');
+  } else {
+    $('#hide1').removeClass('fa-eye').addClass('fa-eye-slash');
+    $('#signup_password').attr('type', 'password');
+  }
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
